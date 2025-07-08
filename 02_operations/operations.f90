206b7176a305ff152a3d62bfb3d667c723d1220d
i i <ii@iiMac0.local>
2025-07-09 02:31:14 +0300

    Update operations.f90

--- 02_operations/operations.f90
+++ 02_operations/operations.f90
@@ -3,12 +3,14 @@ PROGRAM operations
 ! Use variables (all variables are integers)
 INTEGER :: i, j, k
 
+! Get two values to store i and j 
 WRITE (*,*) 'Enter the numbers to multiply: '
 READ (*,*) i, j 
 
-k = i*j 
+k = i*j  ! Multiply numbers
 
 WRITE (*,*) 'Result =', k
 
+! Finish the equation
 STOP
 END PROGRAM operations
